1e091c731645bd21fb5870fcaeb39f3d9c717ff5
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-10-21 07:07:16 +0700

    add database connection

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
 import express from "express";
 import router from "./routes/router.js";
+import db from "./config/db.js";
 
 // express gateaway
 const app = express();
 
 // defining port
 const PORT = process.env.PORT || 5000;
+
+// database connection
+try {
+	await db.authenticate();
+	console.log("Database Connected...");
+} catch (error) {
+	console.error("Connection Failed", error);
+}
+
+// middleware
+app.use(express.json());
 
 // load route
 app.use(router);
